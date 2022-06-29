eb9015a8987a138b02472bcba58719a5bb7a97c2
Irene Martinez <imartinez921@gmail.com>
2022-06-29 19:08:07 -0400

    Complete first-routes-and-controllers-project

--- first-routes-and-controllers/config/routes.rb
+++ first-routes-and-controllers/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'users/:id', to: 'users#show', as: 'user'
   get 'users', to: 'users#index'
   post 'users', to: 'users#create'
-  patch 'users/:id', to:  'users#update'
+  patch 'users/:id', to: 'users#update'
   put 'users/:id', to: 'users#update'
   delete 'users/:id', to: "users#destroy"
 
