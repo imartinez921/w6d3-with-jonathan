fad82156b8bbe4f64cd981c4824ab9d6137e0edd
Irene Martinez <imartinez921@gmail.com>
2022-06-29 14:55:36 -0400

    Initial commit first-routes-and-controllers project with Jonathan

---
+++ first-routes-and-controllers/app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+    def index
+    #     @users = User.all
+    #     render json: @users
+    end
+
+    # def show
+    #     @user = User.find(params[:id])
+    #     render json: @user
+    # end
+
+    # def user_params
+    #     params.require(:user).permit(:placeholder)
+    # end
+
+end
